8d61d468c75a55088df08ec9f9c0edb910afcc82
stefani cortes <stefanicortesc24@gmail.com>
2022-08-17 18:11:45 -0500

    nuevas tecnologias primera clase

---
+++ main.py
@@ -0,0 +1,33 @@
+#entradas del problema
+'''
+numero1=None
+numero2=80
+numero3=100
+
+ciudad="Medeliin"
+print("Tuciudad es  ",ciudad)
+print(f"Tuciudad es  {ciudad} y tu numero es {numero3}")
+
+
+#salidas del  problema
+print("El numero 1 es : ",numero1)
+print("El numero 2 es : ",numero2)
+print("El numero 3 es : ",numero3)
+
+
+#entradas por teclado
+
+nombre=input("Digite su nombre:  ")
+
+print(f'Su nombre es {nombre}')
+'''
+numero1 = int(input(f'Digite el primer numero:  '))
+numero2 = int(input(f'Digite el segundo numero:  '))
+
+suma = numero1 + numero2
+
+print(f'La suma de los numeros es:  {suma}')
+#print(f'El primer numero es: ',numero1)
+#print(f'El segundo numero es: ',numero2)
+
+
---
+++ retos.py
@@ -0,0 +1,20 @@
+'''
+print ('Bienvenido a mi programa')
+
+numero=int(input("Digite un nuemro:  "))
+
+resultado = numero%3
+
+print(f'El modulo del numero digitado es: {resultado}')
+
+numero2 = int(input("Digite el numero:  "))
+resultado = numero2%5
+print(f'El resultado es: {resultado}')
+'''
+
+numero1 = int(input("digite el numero:  "))
+numero2 = int(input("digite el numero:  "))
+
+print(f'El numero 1 es : {numero1}')
+print(f'El numero 2 es : {numero2}')
+
